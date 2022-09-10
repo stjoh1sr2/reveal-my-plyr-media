d6244a0ea13ee0558cc115bda2a13d68b9d8d7b3
Samuel St John <71797482+stjoh1sr2@users.noreply.github.com>
2022-09-10 15:33:57 -0400

    Update description

--- reveal-my-plyr-media/plugin.js
+++ reveal-my-plyr-media/plugin.js
@@ -1,6 +1,7 @@
 /****************************************************************
  * Author: Samuel St John, stjoh1sr@cmich.edu
  * 
- * A reveal.js plugin to interface with Plyr.
+ * A reveal.js plugin to interface with Plyr and allow tracking
+ * on video/audio interactions and events.
  * 
  ****************************************************************/
