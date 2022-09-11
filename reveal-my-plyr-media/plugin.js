8fc36a3f1d662e7a63c9dbc6bafaa40c59ab0624
Samuel St John <stjoh1sr@cmich.edu>
2022-09-11 19:22:40 -0400

    Print details to console

--- reveal-my-plyr-media/plugin.js
+++ reveal-my-plyr-media/plugin.js
@@ -9,25 +9,43 @@
 // Event listeners
 player.addEventListener('pause', (event) => {
 	console.log("You've paused!");
+	consoleLogDetails(event.detail.plyr);
 });
 
 player.addEventListener('playing', (event) => {
-	console.log("You've resumed");
+	console.log("You've now playing!");
+	consoleLogDetails(event.detail.plyr);
 });
-
-player.addEventListener('play', (event) => {
-	console.log("You're now playing!");
-});
-
-/* player.addEventListener('timeupdate', (event) => {
- * 	console.log("The playtime has changed!");
- * });
- */
 
 player.addEventListener('seeking', (event) => {
 	console.log("You're trying to skip around the video!");
+	consoleLogDetails(event.detail.plyr);
 });
 
 player.addEventListener('seeked', (event) => {
 	console.log("You've successfully skipped to a different part of the video!");
+	consoleLogDetails(event.detail.plyr);
 });
+
+// Detail-printing
+function consoleLogDetails(media) {
+	const details = {
+		playing: media.playing,
+		paused: media.paused,
+		ended: media.ended,
+		buffered: media.buffered,
+		currentTime: media.currentTime,
+		seeking: media.seeking,
+		volume: media.volume * 100. + "%",
+		muted: media.muted,
+		speed: media.speed,
+		captionsActive: media.captions.active ? true : false,
+		captionLanguage: media.captions.active ? media.language : "N/A",
+		loop: media.loop,
+		fullscreenActive: media.fullscreen.active,
+		failed: media.failed,
+		currentTime: media.currentTime
+	}
+
+	console.log(details);
+}
