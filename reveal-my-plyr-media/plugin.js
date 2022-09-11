603957713a08417db3ab965d2feb2abb672c083b
Samuel St John <stjoh1sr@cmich.edu>
2022-09-11 18:31:37 -0400

    Add initial work on event listeners and update demo HTML

--- reveal-my-plyr-media/plugin.js
+++ reveal-my-plyr-media/plugin.js
@@ -5,3 +5,33 @@
  * on video/audio interactions and events.
  * 
  ****************************************************************/
+
+// Event listeners
+player.addEventListener('pause', (event) => {
+	console.log("You've paused!");
+});
+
+player.addEventListener('playing', (event) => {
+	console.log("You've resumed");
+});
+
+player.addEventListener('play', (event) => {
+	console.log("You're now playing!");
+});
+
+/* player.addEventListener('timeupdate', (event) => {
+ * 	console.log("The playtime has changed!");
+ * });
+ */
+
+player.addEventListener('timeupdate', (event) => {
+	console.log("The playtime has changed!");
+});
+
+player.addEventListener('seeking', (event) => {
+	console.log("You're trying to skip around the video!");
+});
+
+player.addEventListener('seeked', (event) => {
+	console.log("You've successfully skipped to a different part of the video!");
+});
