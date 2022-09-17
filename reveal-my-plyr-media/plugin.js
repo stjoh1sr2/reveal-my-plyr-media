bead9c92c56cbce9833fdc5e68c9343d67bbf0b0
Samuel St John <stjoh1sr@cmich.edu>
2022-09-17 16:31:55 -0400

    Update event listeners and add source details
    Signed-off-by: Samuel St John <stjoh1sr@cmich.edu>

--- reveal-my-plyr-media/plugin.js
+++ reveal-my-plyr-media/plugin.js
@@ -13,17 +13,27 @@ player.addEventListener('pause', (event) => {
 });
 
 player.addEventListener('playing', (event) => {
-	console.log("You've now playing!");
-	consoleLogDetails(event.detail.plyr);
-});
-
-player.addEventListener('seeking', (event) => {
-	console.log("You're trying to skip around the video!");
+	console.log("You're playing!");
 	consoleLogDetails(event.detail.plyr);
 });
 
 player.addEventListener('seeked', (event) => {
-	console.log("You've successfully skipped to a different part of the video!");
+	console.log("You've successfully skipped around the video!");
+	consoleLogDetails(event.detail.plyr);
+});
+
+player.addEventListener('volumechange', (event) => {
+	console.log("You've changed the volume!");
+	consoleLogDetails(event.detail.plyr);
+});
+
+player.addEventListener('enterfullscreen', (event) => {
+	console.log("You've entered fullscreen mode!");
+	consoleLogDetails(event.detail.plyr);
+});
+
+player.addEventListener('exitfullscreen', (event) => {
+	console.log("You've left fullscreen mode!");
 	consoleLogDetails(event.detail.plyr);
 });
 
@@ -33,12 +43,12 @@ function consoleLogDetails(media) {
 		playing: media.playing,
 		paused: media.paused,
 		ended: media.ended,
-		buffered: media.buffered,
 		currentTime: media.currentTime,
 		seeking: media.seeking,
 		volume: media.volume * 100. + "%",
 		muted: media.muted,
 		speed: media.speed,
+		mediaSource: media.source,
 		captionsActive: media.captions.active ? true : false,
 		captionLanguage: media.captions.active ? media.language : "N/A",
 		loop: media.loop,
