c4893effb8191943fdb62e29ec2ad924e55d957c
Samuel St John <stjoh1sr@cmich.edu>
2022-10-31 16:52:40 -0400

    Remove console.log statements
    Signed-off-by: Samuel St John <stjoh1sr@cmich.edu>

--- reveal-my-plyr-media/plugin.js
+++ reveal-my-plyr-media/plugin.js
@@ -30,34 +30,22 @@ function awaitPlayerInitialization() {
 function initializeListeners() {
 	while (i < players.length) {
 		players[i].on('pause', (event) => {
-			console.log("You've paused!");
 			dispatchPlyrInteraction(event.detail.plyr, 'pause');
 		});
 
 		players[i].on('playing', (event) => {
-			console.log("You're playing!");
 			dispatchPlyrInteraction(event.detail.plyr, 'playing');
 		});
 
 		players[i].on('seeked', (event) => {
-			console.log("You've successfully skipped around the video!");
 			dispatchPlyrInteraction(event.detail.plyr, 'seek');
 		});
 
-		players[i].on('volumechange', (event) => {
-			if (event.detail.currentTime > 0) {
-				console.log("You've changed the volume!");
-				dispatchPlyrInteraction(event.detail.plyr, 'volumeChange');
-			}
-		});
-
 		players[i].on('enterfullscreen', (event) => {
-			console.log("You've entered fullscreen mode!");
 			dispatchPlyrInteraction(event.detail.plyr, 'enterFullscreen');
 		});
 
 		players[i].on('exitfullscreen', (event) => {
-			console.log("You've left fullscreen mode!");
 			dispatchPlyrInteraction(event.detail.plyr, 'exitFullscreen');
 		});
 
